1635d87c4f1b53758a7278c0bf0a1fde3b09c85a
adam2809 <adam.kule@gmail.com>
2019-11-16 16:42:57 +0000

    Forgot file

---
+++ recording.py
@@ -0,0 +1,41 @@
+import cv2 as cv
+import numpy as np
+
+WAIT_FOR_KEY = 17
+
+# Starts on calling the function ends when e pressed
+def recordVideo():
+    cap = cv.VideoCapture(0)
+    recording = []
+    while True:
+        _, frame = cap.read()
+
+        cv.imshow("Frame", frame)
+        recording.append(frame.copy())
+
+        key = cv.waitKey(WAIT_FOR_KEY)
+        if(key == 101):
+            break
+
+    cap.release()
+    cv.destroyAllWindows()
+
+    return recording
+
+
+# Takes snap and returns it on e key press and exits
+def takeSnap():
+    return recordVideo()[-1]
+
+
+def playbackVideo(frames):
+    for f in frames:
+        print(f)
+        cv.imshow("Frame",f)
+        key = cv.waitKey(WAIT_FOR_KEY)
+        if(key == 101):
+            break
+
+
+f = recordVideo()
+playbackVideo(f)
